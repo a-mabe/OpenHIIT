1ebb504bfbe7ea74f772c037f1ef4cdac763da07
a-mabe <mabe.abby.a@gmail.com>
2023-04-27 20:39:25 -0400

    Getting started with a FAB

--- workout-timer/App.js
+++ workout-timer/App.js
@@ -1,20 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FAB } from 'react-native-elements';
 
-export default function App() {
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  const clickHandler = () => {
+    //function to handle click on floating Action Button
+    alert('Floating Button Clicked');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.titleStyle}>
+          Example of React Native Floating Action Button
+        </Text>
+        <Text style={styles.textStyle}>
+          Click on Action Button to see Alert
+        </Text>
+        <FAB
+          icon={{ name: 'add', color: 'white' }}
+          activeOpacity={0.7}
+          onPress={clickHandler}
+          style={styles.touchableOpacityStyle}>
+        </FAB>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  titleStyle: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 10,
+  },
+  textStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+    padding: 10,
+  },
+  touchableOpacityStyle: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
     alignItems: 'center',
     justifyContent: 'center',
+    right: 30,
+    bottom: 30,
+  },
+  floatingButtonStyle: {
+    resizeMode: 'contain',
+    width: 50,
+    height: 50,
+    //backgroundColor:'black'
   },
 });
+
+// import React from 'react';
+// import { View, Text, ScrollView, StyleSheet } from 'react-native';
+// import { FAB } from 'react-native-elements';
+
+// const App = () => {
+//   const [visible, setVisible] = React.useState(true);
+//   return (
+//     <>
+//       <ScrollView>
+//         <FAB
+//           style={styles.floatinBtn}
+//           position="right"
+//           visible={visible}
+//           icon={{ name: 'add', color: 'white' }}
+//           size="small"
+//         />
+//       </ScrollView>
+//     </>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   floatinBtn: {
+//     position: 'fixed',
+//     bottom: 10,
+//     right: 10,
+//   }
+// });
+
+// export default App;
