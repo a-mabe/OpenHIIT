c659d46eda6283a78739b6b89b6fde9307e386d4
a-mabe <mabe.abby.a@gmail.com>
2023-06-18 16:22:41 -0400

    Phone will not sleep while the timer is running

--- workout_timer/macos/Flutter/GeneratedPluginRegistrant.swift
+++ workout_timer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import audioplayers_darwin
 import path_provider_foundation
 import sqflite
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
