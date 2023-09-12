403d5a489a2cc592f2732f3cd60c65e90510a0c1
a-mabe <mabe.abby.a@gmail.com>
2023-09-12 19:53:46 -0400

    iOS release

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    UIApplication.shared.beginReceivingRemoteControlEvents()
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
