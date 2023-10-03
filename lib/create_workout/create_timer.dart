8c0b1866412446df5e2619d8031d3c7a37a164c0
Dhruv Patel <dhruvpatel282.dp@gmail.com>
2023-10-03 19:50:10 -0400

    Fixed submit button overflow bug

--- lib/create_workout/create_timer.dart
+++ lib/create_workout/create_timer.dart
@@ -100,7 +100,8 @@ class _ChooseIntervalsState extends State<ChooseIntervals> {
 
   @override
   Widget build(BuildContext context) {
-    Workout workoutArgument = ModalRoute.of(context)!.settings.arguments as Workout;
+    Workout workoutArgument =
+        ModalRoute.of(context)!.settings.arguments as Workout;
 
     if (!_changed && workoutArgument.numExercises > 0) {
       _currentIntValue = workoutArgument.numExercises;
@@ -109,103 +110,108 @@ class _ChooseIntervalsState extends State<ChooseIntervals> {
 
     return Form(
       key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(40.0, 30.0, 40.0, 0.0),
-            child: Text(
-              "Name this timer:",
-              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0.0),
-            child: TextFormField(
-              textCapitalization: TextCapitalization.sentences,
-              maxLength: 40,
-              initialValue: workoutArgument.title,
-              // The validator receives the text that the user has entered.
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              onSaved: (val) => setState(() => workoutArgument.title = val!),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(40.0, 30.0, 40.0, 0.0),
-            child: Text(
-              "How many intervals?",
-              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Center(
-            child: NumberPicker(
-              value: _currentIntValue,
-              minValue: 1,
-              maxValue: 50,
-              step: 1,
-              haptics: true,
-              onChanged: (value) => setState(() {
-                _currentIntValue = value;
-                _changed = true;
-              }),
-            ),
-          ),
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.remove),
-                  onPressed: () => setState(() {
-                    final newValue0 = _currentIntValue - 1;
-                    _currentIntValue = newValue0.clamp(1, 50);
-                  }),
-                ),
-                Text('Current int value: $_currentIntValue'),
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () => setState(() {
-                    final newValue = _currentIntValue + 1;
-                    _currentIntValue = newValue.clamp(1, 50);
-                  }),
-                ),
-              ],
-            ),
-          ),
-          // Color picker
-          const Padding(
-            padding: EdgeInsets.fromLTRB(40.0, 30.0, 40.0, 0.0),
-            child: Text(
-              "Set timer color:",
-              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: pickColor,
-              child: CircleColor(
-                color: _timerColor,
-                circleSize: MediaQuery.of(context).size.width * 0.15,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(40.0, 30.0, 40.0, 0.0),
+              child: Text(
+                "Name this timer:",
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  submitForm(workoutArgument);
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0.0),
+              child: TextFormField(
+                textCapitalization: TextCapitalization.sentences,
+                maxLength: 40,
+                initialValue: workoutArgument.title,
+                // The validator receives the text that the user has entered.
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
                 },
-                child: const Text('Submit'),
+                onSaved: (val) => setState(() => workoutArgument.title = val!),
               ),
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.fromLTRB(40.0, 30.0, 40.0, 0.0),
+              child: Text(
+                "How many intervals?",
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+              child: NumberPicker(
+                value: _currentIntValue,
+                minValue: 1,
+                maxValue: 50,
+                step: 1,
+                haptics: true,
+                onChanged: (value) => setState(() {
+                  _currentIntValue = value;
+                  _changed = true;
+                }),
+              ),
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.remove),
+                    onPressed: () => setState(() {
+                      final newValue0 = _currentIntValue - 1;
+                      _currentIntValue = newValue0.clamp(1, 50);
+                    }),
+                  ),
+                  Text('Current int value: $_currentIntValue'),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () => setState(() {
+                      final newValue = _currentIntValue + 1;
+                      _currentIntValue = newValue.clamp(1, 50);
+                    }),
+                  ),
+                ],
+              ),
+            ),
+            // Color picker
+            const Padding(
+              padding: EdgeInsets.fromLTRB(40.0, 30.0, 40.0, 0.0),
+              child: Text(
+                "Set timer color:",
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+              child: GestureDetector(
+                onTap: pickColor,
+                child: CircleColor(
+                  color: _timerColor,
+                  circleSize: MediaQuery.of(context).size.width * 0.15,
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    submitForm(workoutArgument);
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
