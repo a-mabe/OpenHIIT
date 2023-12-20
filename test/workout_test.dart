177f69ed7e1b1ae152cbb98e3161d8be1ad0b351
a-mabe <mabe.abby.a@gmail.com>
2023-12-20 13:21:58 -0500

    Update expected form title in tests

--- test/workout_test.dart
+++ test/workout_test.dart
@@ -42,7 +42,7 @@ void main() {
     await tester.pumpAndSettle();
 
     // Verify that the next page has loaded.
-    expect(find.text('Name this workout:'), findsOneWidget);
+    expect(find.text('Enter a name:'), findsOneWidget);
 
     // Fill out the Workout name.
     expect(find.byType(TextFormField), findsOneWidget);
