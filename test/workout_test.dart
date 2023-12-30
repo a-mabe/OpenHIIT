5137c65fbc1fde87c0c9f75ec4ba38d204d6f34a
a-mabe <mabe.abby.a@gmail.com>
2023-12-29 20:48:27 -0500

    Increase pump duration before ViewWorkout screen

--- test/workout_test.dart
+++ test/workout_test.dart
@@ -154,7 +154,7 @@ void main() {
     await tester.tap(find.text(workoutName));
 
     // Wait for the navigation to complete
-    await tester.pump(const Duration(seconds: 1));
+    await tester.pump(const Duration(seconds: 5));
 
     // Verify the ViewWorkout page has loaded
     expect(find.text("Start"), findsOneWidget);
