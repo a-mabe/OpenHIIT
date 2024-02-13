df063a724a995780a92e05725af10671a6bfa9e2
a-mabe <mabe.abby.a@gmail.com>
2024-02-12 19:41:53 -0500

    Remove sizedbox wrap from numberinput

--- lib/create_workout/form_picker_widgets/number_input.dart
+++ lib/create_workout/form_picker_widgets/number_input.dart
@@ -60,49 +60,47 @@ class NumberInputState extends State<NumberInput> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: widget.widgetWidth,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            SizedBox(
-                width: 50,
-                child: TextFormField(
-                  key: widget.numberInputKey,
-                  initialValue: widget.numberValue == -1
-                      ? ""
-                      : widget.formatter(widget.numberValue).toString(),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly,
-                    NumericalRangeFormatter(min: widget.min, max: widget.max),
-                  ],
-                  textInputAction: TextInputAction.done,
-                  style: const TextStyle(fontSize: 30),
-                  textAlign: TextAlign.center,
-                  maxLength: 3,
-                  decoration: const InputDecoration(
-                    hintText: "00",
-                    errorStyle: TextStyle(height: 0.1, fontSize: 10),
-                    errorMaxLines: 1,
-                    border: InputBorder.none,
-                    fillColor: Colors.white,
-                    counterText: "",
-                    contentPadding: EdgeInsets.zero,
-                  ),
-                  onChanged: widget.onChanged,
-                  validator: widget.validator,
-                  onSaved: widget.onSaved,
-                )),
-            Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 3),
-                child: Text(
-                  widget.unit,
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(color: Colors.grey, fontSize: 18),
-                )),
-          ],
-        ));
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+            width: 50,
+            child: TextFormField(
+              key: widget.numberInputKey,
+              initialValue: widget.numberValue == -1
+                  ? ""
+                  : widget.formatter(widget.numberValue).toString(),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly,
+                NumericalRangeFormatter(min: widget.min, max: widget.max),
+              ],
+              textInputAction: TextInputAction.done,
+              style: const TextStyle(fontSize: 30),
+              textAlign: TextAlign.center,
+              maxLength: 3,
+              decoration: const InputDecoration(
+                hintText: "00",
+                errorStyle: TextStyle(height: 0.1, fontSize: 10),
+                errorMaxLines: 1,
+                border: InputBorder.none,
+                fillColor: Colors.white,
+                counterText: "",
+                contentPadding: EdgeInsets.zero,
+              ),
+              onChanged: widget.onChanged,
+              validator: widget.validator,
+              onSaved: widget.onSaved,
+            )),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 3),
+            child: Text(
+              widget.unit,
+              textAlign: TextAlign.left,
+              style: const TextStyle(color: Colors.grey, fontSize: 18),
+            )),
+      ],
+    );
   }
 }
