011f265c98b5adb47a8c4b0c1def11627867cf76
a-mabe <mabe.abby.a@gmail.com>
2024-03-12 16:02:05 -0400

    Revert workout test name

--- test/workout_test.dart
+++ test/workout_test.dart
@@ -19,7 +19,7 @@ void main() {
     String workoutName = "Test workout 1";
 
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const workoutTimer());
+    await tester.pumpWidget(const WorkoutTimer());
 
     // Tap the '+' icon and trigger the add Workout or Timer page.
     await tester.tap(find.byIcon(Icons.add));
