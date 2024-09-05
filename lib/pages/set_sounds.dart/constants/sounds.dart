6a9e22d8bffb61e50416697941040351f199bc63
a-mabe <mabe.abby.a@gmail.com>
2024-09-05 19:05:19 -0400

    Combine sound constants files

--- lib/pages/set_sounds.dart/constants/sounds.dart
+++ lib/pages/set_sounds.dart/constants/sounds.dart
@@ -21,3 +21,22 @@ const List<String> countdownSounds = <String>[
   'short-rest-beep',
   'none',
 ];
+
+var soundNames = {
+  "long-bell": "Long bell",
+  "countdown-beep": "Countdown beep",
+  "ding-sequence": "Ding sequence",
+  "ding": "Ding",
+  "halfway-beep2": "Long beep sequence",
+  "harsh-beep-sequence": "Harsh beep sequence",
+  "harsh-beep": "Harsh beep",
+  "horn": "Horn",
+  "long-halfway-beep": "Blip sequence",
+  "long-rest-beep": "Quick beep sequence",
+  "long-whistle": "Long whistle",
+  "short-halfway-beep": "Blip",
+  "short-rest-beep": "Beep",
+  "short-whistle": "Short whistle",
+  "thunk": "Thunk",
+  "none": "None"
+};
