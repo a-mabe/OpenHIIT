3ad0c9771f4210eb16ebb4887a8b2ea72dcb4a3e
a-mabe <mabe.abby.a@gmail.com>
2024-10-03 19:49:28 -0400

    Update workout type

--- lib/models/workout_type.dart
+++ lib/models/workout_type.dart
@@ -1,302 +1,152 @@
-import 'dart:core';
-
 class Workout {
-  /// The workout ID.
-  ///
-  late String id;
+  String id;
+  String title;
+  int numExercises;
+  String exercises;
+  int getReadyTime;
+  int workTime;
+  int restTime;
+  int halfTime;
+  int breakTime;
+  int warmupTime;
+  int cooldownTime;
+  int iterations;
+  int halfwayMark;
+  String workSound;
+  String restSound;
+  String halfwaySound;
+  String completeSound;
+  String countdownSound;
+  int colorInt;
+  int workoutIndex;
+  int showMinutes;
 
-  /// The name of the to-do item.
-  ///
-  /// e.g., "Tuesday Workout"
-  ///
-  late String title;
+  // Main constructor
+  Workout({
+    required this.id,
+    required this.title,
+    required this.numExercises,
+    required this.exercises,
+    this.getReadyTime = 10,
+    this.workTime = 0,
+    this.restTime = 0,
+    this.halfTime = 0,
+    this.breakTime = 0,
+    this.warmupTime = 0,
+    this.cooldownTime = 0,
+    this.iterations = 0,
+    this.halfwayMark = 0,
+    this.workSound = "short-whistle",
+    this.restSound = "short-rest-beep",
+    this.halfwaySound = "short-halfway-beep",
+    this.completeSound = "long-bell",
+    this.countdownSound = "countdown-beep",
+    this.colorInt = 4280391411,
+    this.workoutIndex = 0,
+    this.showMinutes = 0,
+  });
 
-  /// The number of exercises.
-  ///
-  /// e.g., "I need to do X but do it in Y way"
-  ///
-  late int numExercises;
+  // Named constructor for empty workouts
+  Workout.empty()
+      : id = "",
+        title = "",
+        numExercises = 0,
+        exercises = "",
+        getReadyTime = 10,
+        workTime = 0,
+        restTime = 0,
+        halfTime = 0,
+        breakTime = 0,
+        warmupTime = 0,
+        cooldownTime = 0,
+        iterations = 0,
+        halfwayMark = 0,
+        workSound = "short-whistle",
+        restSound = "short-rest-beep",
+        halfwaySound = "short-halfway-beep",
+        completeSound = "long-bell",
+        countdownSound = "countdown-beep",
+        colorInt = 4280391411,
+        workoutIndex = 0,
+        showMinutes = 0;
 
-  /// List of the exercises, JSON encoded string.
-  ///
-  /// e.g., ["Burpee", "Push-ups", "Rows"]
-  ///
-  late String exercises;
+  // Create a copy
+  Workout copy() => Workout(
+        id: id,
+        title: title,
+        numExercises: numExercises,
+        exercises: exercises,
+        getReadyTime: getReadyTime,
+        workTime: workTime,
+        restTime: restTime,
+        halfTime: halfTime,
+        breakTime: breakTime,
+        warmupTime: warmupTime,
+        cooldownTime: cooldownTime,
+        iterations: iterations,
+        halfwayMark: halfwayMark,
+        workSound: workSound,
+        restSound: restSound,
+        halfwaySound: halfwaySound,
+        completeSound: completeSound,
+        countdownSound: countdownSound,
+        colorInt: colorInt,
+        workoutIndex: workoutIndex,
+        showMinutes: showMinutes,
+      );
 
-  /// Amount of time to count down to the timer start, in seconds.
-  ///
-  /// e.g., 10
-  ///
-  late int getReadyTime;
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'title': title,
+        'numExercises': numExercises,
+        'exercises': exercises,
+        'getReadyTime': getReadyTime,
+        'exerciseTime': workTime,
+        'restTime': restTime,
+        'halfTime': halfTime,
+        'breakTime': breakTime,
+        'warmupTime': warmupTime,
+        'cooldownTime': cooldownTime,
+        'iterations': iterations,
+        'halfwayMark': halfwayMark,
+        'workSound': workSound,
+        'restSound': restSound,
+        'halfwaySound': halfwaySound,
+        'completeSound': completeSound,
+        'countdownSound': countdownSound,
+        'colorInt': colorInt,
+        'workoutIndex': workoutIndex,
+        'showMinutes': showMinutes,
+      };
 
-  /// Amount of time for an exercise, in seconds.
-  ///
-  /// e.g., 30
-  ///
-  late int workTime;
+  Workout.fromMap(Map<String, dynamic> map)
+      : id = map['id'] ?? "",
+        title = map['title'] ?? "",
+        numExercises = map['numExercises'] ?? 0,
+        exercises = map['exercises'] ?? "",
+        getReadyTime = map['getReadyTime'] ?? 10,
+        workTime = map['exerciseTime'] ?? 0,
+        restTime = map['restTime'] ?? 0,
+        halfTime = map['halfTime'] ?? 0,
+        breakTime = map['breakTime'] ?? 0,
+        warmupTime = map['warmupTime'] ?? 0,
+        cooldownTime = map['cooldownTime'] ?? 0,
+        iterations = map['iterations'] ?? 0,
+        halfwayMark = map['halfwayMark'] ?? 0,
+        workSound = map['workSound'] ?? "short-whistle",
+        restSound = map['restSound'] ?? "short-rest-beep",
+        halfwaySound = map['halfwaySound'] ?? "short-halfway-beep",
+        completeSound = map['completeSound'] ?? "long-bell",
+        countdownSound = map['countdownSound'] ?? "countdown-beep",
+        colorInt = map['colorInt'] ?? 4280391411,
+        workoutIndex = map['workoutIndex'] ?? 0,
+        showMinutes = map['showMinutes'] ?? 0;
 
-  /// Amount of time between exercises, in seconds. (Rest time)
-  ///
-  /// e.g., 30
-  ///
-  late int restTime;
+  // Convert to JSON
+  Map<String, dynamic> toJson() => toMap();
 
-  /// Amount of time within an exercise to change sides, in seconds. (Half time)
-  ///
-  /// e.g., 5
-  ///
-  late int halfTime;
-
-  /// Amount of time for breaks between exercise cycles, in seconds.
-  ///
-  /// e.g., 60
-  ///
-  late int breakTime;
-
-  /// Amount of time dedicated to warm-up before the exercise routine starts, in seconds.
-  ///
-  /// e.g., 120
-  ///
-  late int warmupTime;
-
-  /// Amount of time dedicated to cooldown after completing the exercise routine, in seconds.
-  ///
-  /// e.g., 90
-  ///
-  late int cooldownTime;
-
-  /// The total number of exercise cycles or iterations in the routine.
-  ///
-  /// e.g., 5
-  ///
-  late int iterations;
-
-  /// The time mark, in seconds, at which the exercise routine is considered halfway completed.
-  ///
-  /// e.g., 300
-  ///
-  late int halfwayMark;
-
-  /// The sound file associated with the work phase of the exercise routine.
-  ///
-  /// e.g., "work_sound.mp3"
-  ///
-  late String workSound;
-
-  /// The sound file associated with the rest phase of the exercise routine.
-  ///
-  /// e.g., "rest_sound.mp3"
-  ///
-  late String restSound;
-
-  /// The sound file played at the halfway mark of the exercise routine.
-  ///
-  /// e.g., "halfway_sound.mp3"
-  ///
-  late String halfwaySound;
-
-  /// The sound file played upon completing the entire exercise routine.
-  ///
-  /// e.g., "complete_sound.mp3"
-  ///
-  late String completeSound;
-
-  /// The sound file played during countdowns or preparations.
-  ///
-  /// e.g., "countdown_sound.mp3"
-  ///
-  late String countdownSound;
-
-  /// Color selected for the background of the workout
-  ///
-  /// e.g., 456787899
-  ///
-  late int colorInt;
-
-  /// Index of the workout
-  ///
-  /// e.g., 6
-  ///
-  late int workoutIndex;
-
-  /// Whether the timer should display minutes.
-  ///
-  /// e.g., 0 for false
-  ///
-  late int showMinutes;
-
-  Workout(
-      this.id,
-      this.title,
-      this.numExercises,
-      this.exercises,
-      this.getReadyTime,
-      this.workTime,
-      this.restTime,
-      this.halfTime,
-      this.breakTime,
-      this.warmupTime,
-      this.cooldownTime,
-      this.iterations,
-      this.halfwayMark,
-      this.workSound,
-      this.restSound,
-      this.halfwaySound,
-      this.completeSound,
-      this.countdownSound,
-      this.colorInt,
-      this.workoutIndex,
-      this.showMinutes);
-
-  Workout.empty() {
-    id = "";
-    title = "";
-    numExercises = 0;
-    exercises = "";
-    getReadyTime = 10;
-    workTime = 0;
-    restTime = 0;
-    halfTime = 0;
-    breakTime = 0;
-    warmupTime = 0;
-    cooldownTime = 0;
-    iterations = 0;
-    halfwayMark = 0;
-    workSound = "short-whistle";
-    restSound = "short-rest-beep";
-    halfwaySound = "short-halfway-beep";
-    countdownSound = "countdown-beep";
-    completeSound = "long-bell";
-    colorInt = 4280391411;
-    workoutIndex = 0;
-    showMinutes = 0;
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'title': title,
-      'numExercises': numExercises,
-      'exercises': exercises,
-      'getReadyTime': getReadyTime,
-      'exerciseTime': workTime,
-      'restTime': restTime,
-      'halfTime': halfTime,
-      'breakTime': breakTime,
-      'warmupTime': warmupTime,
-      'cooldownTime': cooldownTime,
-      'iterations': iterations,
-      'halfwayMark': halfwayMark,
-      'workSound': workSound,
-      'restSound': restSound,
-      'halfwaySound': halfwaySound,
-      'completeSound': completeSound,
-      'countdownSound': countdownSound,
-      'colorInt': colorInt,
-      'workoutIndex': workoutIndex,
-      'showMinutes': showMinutes,
-    };
-  }
-
-  Workout.fromMap(Map<String, dynamic> map) {
-    id = map['id'];
-    title = map['title'];
-    numExercises = map['numExercises'];
-    exercises = map['exercises'];
-    getReadyTime = map['getReadyTime'];
-    workTime = map['exerciseTime'];
-    restTime = map['restTime'];
-    halfTime = map['halfTime'];
-    breakTime = map['breakTime'];
-    warmupTime = map['warmupTime'];
-    cooldownTime = map['cooldownTime'];
-    iterations = map['iterations'];
-    halfwayMark = map['halfwayMark'];
-    workSound = map['workSound'];
-    restSound = map['restSound'];
-    halfwaySound = map['halfwaySound'];
-    completeSound = map['completeSound'];
-    countdownSound = map['countdownSound'];
-    colorInt = map['colorInt'];
-    workoutIndex = map['workoutIndex'];
-    showMinutes = map['showMinutes'];
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'title': title,
-      'numExercises': numExercises,
-      'exercises': exercises,
-      'getReadyTime': getReadyTime,
-      'exerciseTime': workTime,
-      'restTime': restTime,
-      'halfTime': halfTime,
-      'breakTime': breakTime,
-      'warmupTime': warmupTime,
-      'cooldownTime': cooldownTime,
-      'iterations': iterations,
-      'halfwayMark': halfwayMark,
-      'workSound': workSound,
-      'restSound': restSound,
-      'halfwaySound': halfwaySound,
-      'completeSound': completeSound,
-      'countdownSound': countdownSound,
-      'colorInt': colorInt,
-      'workoutIndex': workoutIndex,
-      'showMinutes': showMinutes,
-    };
-  }
-
-  Workout.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    title = json['title'];
-    numExercises = json['numExercises'];
-    exercises = json['exercises'];
-    getReadyTime = json['getReadyTime'];
-    workTime = json['exerciseTime'];
-    restTime = json['restTime'];
-    halfTime = json['halfTime'];
-    breakTime = json['breakTime'];
-    warmupTime = json['warmupTime'];
-    cooldownTime = json['cooldownTime'];
-    iterations = json['iterations'];
-    halfwayMark = json['halfwayMark'];
-    workSound = json['workSound'];
-    restSound = json['restSound'];
-    halfwaySound = json['halfwaySound'];
-    completeSound = json['completeSound'];
-    countdownSound = json['countdownSound'];
-    colorInt = json['colorInt'];
-    workoutIndex = json['workoutIndex'];
-    showMinutes = json['showMinutes'];
-  }
-
-  Workout copy() {
-    return Workout(
-      id,
-      title,
-      numExercises,
-      exercises,
-      getReadyTime,
-      workTime,
-      restTime,
-      halfTime,
-      breakTime,
-      warmupTime,
-      cooldownTime,
-      iterations,
-      halfwayMark,
-      workSound,
-      restSound,
-      halfwaySound,
-      completeSound,
-      countdownSound,
-      colorInt,
-      workoutIndex,
-      showMinutes,
-    );
-  }
+  // Create from JSON
+  factory Workout.fromJson(Map<String, dynamic> json) => Workout.fromMap(json);
 
   Workout copyWith({
     String? id,
@@ -322,41 +172,32 @@ class Workout {
     int? showMinutes,
   }) {
     return Workout(
-      id ?? this.id,
-      title ?? this.title,
-      numExercises ?? this.numExercises,
-      exercises ?? this.exercises,
-      getReadyTime ?? this.getReadyTime,
-      workTime ?? this.workTime,
-      restTime ?? this.restTime,
-      halfTime ?? this.halfTime,
-      breakTime ?? this.breakTime,
-      warmupTime ?? this.warmupTime,
-      cooldownTime ?? this.cooldownTime,
-      iterations ?? this.iterations,
-      halfwayMark ?? this.halfwayMark,
-      workSound ?? this.workSound,
-      restSound ?? this.restSound,
-      halfwaySound ?? this.halfwaySound,
-      completeSound ?? this.completeSound,
-      countdownSound ?? this.countdownSound,
-      colorInt ?? this.colorInt,
-      workoutIndex ?? this.workoutIndex,
-      showMinutes ?? this.showMinutes,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      numExercises: numExercises ?? this.numExercises,
+      exercises: exercises ?? this.exercises,
+      getReadyTime: getReadyTime ?? this.getReadyTime,
+      workTime: workTime ?? this.workTime,
+      restTime: restTime ?? this.restTime,
+      halfTime: halfTime ?? this.halfTime,
+      breakTime: breakTime ?? this.breakTime,
+      warmupTime: warmupTime ?? this.warmupTime,
+      cooldownTime: cooldownTime ?? this.cooldownTime,
+      iterations: iterations ?? this.iterations,
+      halfwayMark: halfwayMark ?? this.halfwayMark,
+      workSound: workSound ?? this.workSound,
+      restSound: restSound ?? this.restSound,
+      halfwaySound: halfwaySound ?? this.halfwaySound,
+      completeSound: completeSound ?? this.completeSound,
+      countdownSound: countdownSound ?? this.countdownSound,
+      colorInt: colorInt ?? this.colorInt,
+      workoutIndex: workoutIndex ?? this.workoutIndex,
+      showMinutes: showMinutes ?? this.showMinutes,
     );
   }
 
-  /// Implement toString to print information about
-  /// each Workout more easily.
-  ///
   @override
   String toString() {
     return 'Workout{id: $id, title: $title, numExercises: $numExercises, exercises: $exercises, getReadyTime: $getReadyTime, workTime: $workTime, restTime: $restTime, halfTime: $halfTime, halfwayMark: $halfwayMark, colorInt: $colorInt, index: $workoutIndex, warmupTime: $warmupTime, cooldownTime: $cooldownTime, breakTime: $breakTime, iterations: $iterations}';
   }
-
-  ///
-  /// -------------
-  /// END FUNCTIONS
-  /// -------------
-  ///
 }
