48db8ec5e4b8c210217b2d825d4b756e69f8cd85
a-mabe <mabe.abby.a@gmail.com>
2024-12-27 16:22:41 -0500

    Complete integration tests

--- integration_test/timer_test.dart
+++ integration_test/timer_test.dart
@@ -1,21 +1,53 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:openhiit/main.dart';
 import 'package:integration_test/integration_test.dart';
+
+import 'functions.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  const String workoutName = 'Test Workout';
+  const String workoutName2 = 'Test Workout Edited';
+  const String timerName = 'Test Timer';
+
   group('end-to-end test', () {
-    testWidgets('tap on the floating action button, verify counter',
-        (tester) async {
-      // Load app widget.
-      await tester.pumpWidget(const WorkoutTimer());
-
-      // Wait for the text to appear.
-      await tester.pumpAndSettle(const Duration(seconds: 15));
-
-      // Verify the counter starts at 0.
+    testWidgets('verify app load', (tester) async {
+      await loadApp(tester);
       expect(find.text('No saved timers'), findsOneWidget);
+    });
+    testWidgets('create a workout', (tester) async {
+      await loadApp(tester);
+      await navigateToAddWorkoutOrTimer(tester, true);
+      await createWorkout(tester, workoutName);
+    });
+    testWidgets('run a workout and restart', (tester) async {
+      await loadApp(tester);
+      await verifyWorkoutOrTimerOpens(tester, workoutName);
+      await runWorkoutOne(tester);
+    });
+    testWidgets('edit workout', (tester) async {
+      await loadApp(tester);
+      await verifyWorkoutOrTimerOpens(tester, workoutName);
+      await editWorkout(tester, workoutName2);
+    });
+    testWidgets('run an edited workout and restart', (tester) async {
+      await loadApp(tester);
+      await verifyWorkoutOrTimerOpens(tester, workoutName2);
+      await runWorkoutTwo(tester);
+    });
+    testWidgets('create a timer', (tester) async {
+      await loadApp(tester);
+      await navigateToAddWorkoutOrTimer(tester, false);
+      await createTimer(tester, timerName);
+    });
+    testWidgets('run timer and cancel timer', (tester) async {
+      await loadApp(tester);
+      await verifyWorkoutOrTimerOpens(tester, timerName);
+      await runTimerOne(tester);
+    });
+    testWidgets('delete timer', (tester) async {
+      await loadApp(tester);
+      await deleteWorkoutOrTimer(tester, timerName);
     });
   });
 }
