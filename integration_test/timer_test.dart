ba75185eca1c79c93130c8cd37076992430051f5
a-mabe <mabe.abby.a@gmail.com>
2024-12-29 15:17:11 -0500

    test

--- integration_test/timer_test.dart
+++ integration_test/timer_test.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'functions.dart';
 
@@ -14,10 +16,19 @@ void main() {
   const String timerName = 'Test Timer';
 
   group('end-to-end test', () {
+    // setUp(() {
+    //   Permission.scheduleExactAlarm.
+    // });
     testWidgets('verify app load', (tester) async {
       await loadApp(tester);
       // await binding.setSurfaceSize(const Size(1080, 2400));
       expect(find.text('No saved timers'), findsOneWidget);
+      if (Platform.isAndroid) {
+        await binding.convertFlutterSurfaceToImage();
+        await tester.pumpAndSettle();
+      }
+      final path = await binding.takeScreenshot('screenshot_name');
+      print('Screenshot saved at: $path');
     });
     testWidgets('create a workout', (tester) async {
       await loadApp(tester);
--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,19 +1,19 @@
 import 'dart:io';
 
-import 'package:integration_test/integration_test_driver.dart';
+import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
-  await Process.run(
-    'adb',
-    [
-      'shell',
-      'appops',
-      'set',
-      'com.codepup.workout_timer',
-      'android.permission.SCHEDULE_EXACT_ALARM',
-      'allow',
-    ],
-  );
+  // await Process.run(
+  //   'adb',
+  //   [
+  //     'shell',
+  //     'appops',
+  //     'set',
+  //     'com.codepup.workout_timer',
+  //     'android.permission.SCHEDULE_EXACT_ALARM',
+  //     'allow',
+  //   ],
+  // );
   // await Process.run(
   //   'adb',
   //   [
@@ -34,5 +34,13 @@ Future<void> main() async {
   //     'android.permission.FOREGROUND_SERVICE',
   //   ],
   // );
-  await integrationDriver();
+  await integrationDriver(
+    onScreenshot: (String name, List<int> bytes,
+        [Map<String, Object?>? args]) async {
+      final File image =
+          await File('screenshots/$name.png').create(recursive: true);
+      image.writeAsBytesSync(bytes);
+      return true;
+    },
+  );
 }
