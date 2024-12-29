b08bd40ea4c0c59eea87deb040552d684b7dc8fc
a-mabe <mabe.abby.a@gmail.com>
2024-12-29 12:07:01 -0500

    test

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,3 +1,37 @@
+import 'dart:io';
+
 import 'package:integration_test/integration_test_driver.dart';
 
-Future<void> main() => integrationDriver();
+Future<void> main() async {
+  await Process.run(
+    'adb',
+    [
+      'shell',
+      'pm',
+      'grant',
+      'com.codepup.workout_timer',
+      'android.permission.SCHEDULE_EXACT_ALARM',
+    ],
+  );
+  // await Process.run(
+  //   'adb',
+  //   [
+  //     'shell',
+  //     'pm',
+  //     'grant',
+  //     'com.codepup.workout_timer',
+  //     'android.permission.FOREGROUND_SERVICE_MEDIA_PLAYBACK',
+  //   ],
+  // );
+  // await Process.run(
+  //   'adb',
+  //   [
+  //     'shell',
+  //     'pm',
+  //     'grant',
+  //     'com.codepup.workout_timer',
+  //     'android.permission.FOREGROUND_SERVICE',
+  //   ],
+  // );
+  await integrationDriver();
+}
