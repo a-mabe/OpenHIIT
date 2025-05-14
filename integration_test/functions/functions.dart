48cdbe0aa5b805418e391214500d6043f5083623
a-mabe <mabe.abby.a@gmail.com>
2025-05-13 20:19:15 -0400

    Reduce wait time

--- integration_test/functions/functions.dart
+++ integration_test/functions/functions.dart
@@ -55,7 +55,7 @@ Future<void> setTimings(WidgetTester tester, String workTime, String restTime,
     await tester.tap(find.byType(ExpansionTile).first);
     await tester.pumpAndSettle();
     const timings = {
-      'get-ready-seconds': '40',
+      'get-ready-seconds': '10',
       'cooldown-seconds': '30',
       'warmup-seconds': '20',
       'iterations': '2',
@@ -332,7 +332,7 @@ Future<void> runTimerOne(WidgetTester tester) async {
   expect(find.textContaining("Get Ready"), findsOneWidget);
   expect(find.textContaining("Warmup"), findsOneWidget);
 
-  for (int i = 0; i < 65; i++) {
+  for (int i = 0; i < 61; i++) {
     print("LOG --- waiting for timer to run");
     await tester.pump(const Duration(seconds: 1));
   }
