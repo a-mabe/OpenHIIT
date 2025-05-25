b5d674bc1110302083d7c67094e0b6d920bd2002
a-mabe <mabe.abby.a@gmail.com>
2025-05-25 15:32:18 -0400

    update integration tests

--- integration_test/timer_test.dart
+++ integration_test/timer_test.dart
@@ -1,46 +1,35 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:integration_test/integration_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
-// import 'functions/functions.dart';
+import 'functions/functions.dart';
 
-// void main() {
-//   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-//   binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
 
-//   const String timerName = 'Test Timer';
-
-//   group('end-to-end test', () {
-//     testWidgets('create a timer', (tester) async {
-//       await loadApp(tester);
-//       await navigateToAddWorkoutOrTimer(tester, false);
-//       await createTimer(tester, timerName);
-//     });
-//     // testWidgets('check timer settings', (tester) async {
-//     //   await loadApp(tester);
-//     //   await verifyWorkoutOrTimerOpens(tester, timerName);
-//     //   await checkWorkoutOrTimer(tester, timerName, 1, false, {
-//     //     "10": 2,
-//     //     "40": 1,
-//     //     "30": 1,
-//     //     "90": 1,
-//     //     "20": 1,
-//     //     "2": 1
-//     //   }, {
-//     //     "work-sound": "None",
-//     //     "rest-sound": "None",
-//     //     "halfway-sound": "None",
-//     //     "countdown-sound": "None",
-//     //     "end-sound": "None",
-//     //   });
-//     // });
-//     // testWidgets('run timer and cancel timer', (tester) async {
-//     //   await loadApp(tester);
-//     //   await verifyWorkoutOrTimerOpens(tester, timerName);
-//     //   await runTimerOne(tester);
-//     // });
-//     // testWidgets('delete timer', (tester) async {
-//     //   await loadApp(tester);
-//     //   await deleteWorkoutOrTimer(tester, timerName);
-//     // });
-//   });
-// }
+  group('end-to-end test', () {
+    testWidgets('timer with work and rest', (tester) async {
+      await loadApp(tester);
+      expect(find.text('No saved timers'), findsOneWidget);
+      await tapCreateTimerButton(tester);
+      await pickTimerType(tester, false);
+      await enterTimerName(tester, 'Test Timer');
+      await pickColor(tester);
+      await enterInterval(tester, 4);
+      await tapSubmit(tester);
+      await enterTime(tester, "work-seconds", "8");
+      await enterTime(tester, "rest-seconds", "5");
+      await tapSubmit(tester);
+      await selectSound(tester, "work-sound", "Harsh beep sequence");
+      await tapSubmit(tester);
+      await openViewTimer(tester, 'Test Timer');
+      await tapStartButton(tester);
+      await waitForText(tester, "Get Ready");
+      await waitForText(tester, "1 of 4");
+      await waitForText(tester, "2 of 4");
+      await waitForText(tester, "3 of 4");
+      await waitForText(tester, "4 of 4");
+      await waitForText(tester, "Nice job!");
+    });
+  });
+}
