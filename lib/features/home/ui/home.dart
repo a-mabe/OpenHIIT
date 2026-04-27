055daac7757265a13a4f1137d153630b1c7fb1cd
a-mabe <mabe.abby.a@gmail.com>
2026-04-27 19:19:03 -0400

    fix: tablet portrait mode was showing all navigation
    Only the Navrail should be shown on tablets in portrait mode.

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -119,7 +119,7 @@ class _ListTimersPageState extends State<ListTimersPage> {
                         elevation: 0,
                       ),
                     )
-                  : (!isLandscape ? ListTimersAppBar() : null),
+                  : (!isLandscape && !isTablet ? ListTimersAppBar() : null),
               body: isLandscape || isTablet
                   ? Row(
                       children: [
@@ -135,7 +135,7 @@ class _ListTimersPageState extends State<ListTimersPage> {
                   : SafeArea(
                       child: _buildTimerList(timers),
                     ),
-              bottomNavigationBar: !isLandscape
+              bottomNavigationBar: !isLandscape && !isTablet
                   ? SafeArea(
                       top: false,
                       child: _buildBottomNavBar(timers),
